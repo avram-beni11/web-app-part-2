2780a2816189d919cadbccc647a2b4885e236514
adelmar526 <adelmar526@gmail.com>
2023-01-05 15:24:25 +0000

    server update using express

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var http = require("http"); // requires the built in http module
+var express = require("express"); // requires built in express module
+var app = express();
 
-function requestHandler(req, res) { // req(request), res(response)
-    console.log("In comes the request to:" + req.url);
-    res.end("Welcome");
-}
-var server = http.createServer(requestHandler);
-
+app.use(function(req, res, next) { // req(request), res(response)
+        console.log("In comes the request to:" + req.url);
+        res.end("Welcome");
+        next(); // will send express to the next function.
+    })
+    //creates the server which has the finction to handle requests.
+http.createServer(app).listen(3000);
+//starts the  server listening on port 3000
 server.listen(3000);
