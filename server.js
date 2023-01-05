2266bcd62af571e09f26d413715f63726ebe1bfb
avram-beni11 <avrambeni11@gmail.com>
2023-01-05 19:06:02 +0000

    added basic routing

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-var http = require("http"); // requires the built in http module
-var express = require("express"); // requires built in express module
-var app = express();
+const express = require('express');
+const app = express();
 
-app.use(function(req, res, next) { // req(request), res(response)
-        console.log("In comes the request to:" + req.url);
-        res.end("Welcome");
-        next(); // will send express to the next function.
-    })
-    //creates the server which has the finction to handle requests and starts the  server listening on port 3000.
-http.createServer(app).listen(3000);
+// Define the lessons array
+const lessons = [
+  { id: 1, title: 'Lesson 1' },
+  { id: 2, title: 'Lesson 2' },
+  { id: 3, title: 'Lesson 3' }
+];
+
+app.get('/lessons', (req, res) => {
+  // Generate HTML using the lessons array
+  const html = `
+    <ul>
+      ${lessons.map(lesson => `<li>${lesson.title}</li>`).join('')}
+    </ul>
+  `;
+
+  // Send the HTML as the response
+  res.send(html);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
