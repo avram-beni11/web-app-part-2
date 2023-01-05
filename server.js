667f33eab3d92cc737b1b8a169edb1d965face7f
adelmar526 <adelmar526@gmail.com>
2023-01-05 15:52:07 +0000

    update server.js

--- server.js
+++ server.js
@@ -7,7 +7,5 @@ app.use(function(req, res, next) { // req(request), res(response)
         res.end("Welcome");
         next(); // will send express to the next function.
     })
-    //creates the server which has the finction to handle requests.
+    //creates the server which has the finction to handle requests and starts the  server listening on port 3000.
 http.createServer(app).listen(3000);
-//starts the  server listening on port 3000
-server.listen(3000);
